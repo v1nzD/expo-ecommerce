4b271dcc5ce9df9630b6f245b2b697f54acec749
Vince <vincedimatatac@gmail.com>
2026-02-20 13:54:17 +0800

    created addAddress controller + admin controller fix

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,47 @@
-export async function addAddress(req, res) {}
+import { User } from "../models/user.model.js";
+
+export async function addAddress(req, res) {
+  try {
+    const {
+      label,
+      fullName,
+      streetAddress,
+      city,
+      state,
+      zipCode,
+      phoneNumber,
+      isDefault,
+    } = req.body;
+
+    const user = req.user;
+
+    // if this is set as default, unset all other defaults => only 1 default at a time
+    if (isDefault) {
+      user.addresses.forEach((addr) => (addr.isDefault = false));
+    }
+
+    user.addresses.push({
+      label,
+      fullName,
+      streetAddress,
+      city,
+      state,
+      zipCode,
+      phoneNumber,
+      isDefault: isDefault || false,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "Address added successfully",
+      addresses: user.addresses,
+    });
+  } catch (error) {
+    console.error("Error in addAddress controller:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
 export async function getAddresses(req, res) {}
 export async function updateAddress(req, res) {}
 export async function deleteAddress(req, res) {}
