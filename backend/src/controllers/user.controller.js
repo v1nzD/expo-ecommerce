7e35a569d696db20b9523e9c6eae95e969d3f889
Vince <vincedimatatac@gmail.com>
2026-02-20 16:07:26 +0800

    created getAddress and updateAddress controllers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -42,8 +42,63 @@ export async function addAddress(req, res) {
     res.status(500).json({ error: "Internal server error" });
   }
 }
-export async function getAddresses(req, res) {}
-export async function updateAddress(req, res) {}
+export async function getAddresses(req, res) {
+  try {
+    const user = req.user;
+
+    res.status(200).json({ addresses: user.addresses });
+  } catch (error) {
+    console.error("Error in getAddAddresses controller:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export async function updateAddress(req, res) {
+  try {
+    const {
+      label,
+      fullName,
+      streetAddress,
+      city,
+      state,
+      zipCode,
+      phoneNumber,
+      isDefault,
+    } = req.body;
+
+    const { addressId } = req.params;
+
+    const user = req.user;
+    const address = user.adresses.id(addressId);
+    if (!address) {
+      res.status(404).json({ error: "Address not found" });
+    }
+
+    // if this is set as default, unset all other defaults => only 1 default at a time
+    if (isDefault) {
+      user.addresses.forEach((addr) => (addr.isDefault = false));
+    }
+
+    address.label = label || address.label;
+    address.fullName = fullName || address.fullName;
+    address.streetAddress = streetAddress || address.streetAddress;
+    address.city = city || address.city;
+    address.state = state || address.state;
+    address.zipCode = zipCode || address.zipCode;
+    address.phoneNumber = phoneNumber || address.phoneNumber;
+    address.isDefault = isDefault !== undefined ? isDefault : address.isDefault;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({
+        message: "Address updated successfully",
+        addresses: user.addresses,
+      });
+  } catch (error) {}
+}
+
 export async function deleteAddress(req, res) {}
 export async function addToWishList(req, res) {}
 export async function removeFromWishList(req, res) {}
