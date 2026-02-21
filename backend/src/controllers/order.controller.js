201fb6aad704b26bb5ca2cea160c267be1156739
Vince <vincedimatatac@gmail.com>
2026-02-21 17:37:01 +0800

    implemented createOrder and getUsersOrder controller functions

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,3 +1,78 @@
-export async function createOrder(req, res) {}
+import { Product } from "../models/product.model.js";
+import { Order } from "../models/order.model.js";
+import { Review } from "../models/review.model.js";
 
-export async function getUserOrders(req, res) {}
+export async function createOrder(req, res) {
+  try {
+    const user = req.user;
+    const { orderItems, shippingAddress, paymentResult, totalPrice } = req.body;
+
+    if (!orderItems || orderItems.length === 0) {
+      return res.status(400).json({ error: "No order items" });
+    }
+
+    // validate products and stock
+    for (const item of orderItems) {
+      const product = await Product.findById(item.product._id);
+
+      if (!product) {
+        return res
+          .status(404)
+          .json({ error: `Product ${item.name} not found` });
+      }
+
+      if (product.stock < item.quantity) {
+        return res
+          .status(404)
+          .json({ error: `Insufficient stock for ${product.name}` });
+      }
+    }
+
+    const order = await Order.create({
+      user: user._id,
+      clerkId: user.clerkId,
+      orderItems,
+      shippingAddress,
+      paymentResult,
+      totalPrice,
+    });
+
+    // update product stock: reduce the number of stock for the items in order items when an order is created
+    for (const item of orderItems) {
+      await Product.findByIdAndUpdate(item.product._id, {
+        $inc: { stock: -item.quantity },
+      });
+    }
+
+    res.status(201).json({ message: "Order created successfully", order });
+  } catch (error) {
+    console.error("Error creating order", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getUserOrders(req, res) {
+  try {
+    // fetch all orders for the current authenticated user
+    const clerkId = req.user.clerkId;
+    const orders = await Order.find({ clerkId: clerkId })
+      .populate("orderItems.product")
+      .sort({ createdAt: -1 });
+
+    // check if each order has been reviewed
+    const ordersWithReviewStatus = await Promise.all(
+      orders.map(async (order) => {
+        const review = await Review.findOne({ orderId: order._id });
+        return {
+          ...order.toObject(),
+          hasReviewed: !!review,
+        };
+      }),
+    );
+
+    res.status(200).json({ orders: ordersWithReviewStatus });
+  } catch (error) {
+    console.error("Error in getUserOrders", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -4,4 +4,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 const orderRouter = Router();
 orderRouter.use(protectRoute);
 
+orderRouter.post("/", createOrder);
+orderRouter.get("/", getUserOrders);
+
 export default orderRouter;
