7bcc67af9e6aaeae47c10012bb774e67e571cd6c
vince <vincedimatatac@gmail.com>
2026-02-24 12:21:45 +0800

    implemented getProductById controller

---
+++ backend/src/controllers/product.controller.js
@@ -0,0 +1,17 @@
+import { Product } from "../models/product.model.js";
+
+export async function getProductById(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error in getProductById", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts } from "../controllers/admin.controller.js";
+import { getProductById } from "../controllers/product.controller.js";
 const productRouter = Router();
+
+productRouter.use(protectRoute);
+
+productRouter.get("/", getAllProducts);
+productRouter.get("/:id", getProductById);
 
 export default productRouter;
