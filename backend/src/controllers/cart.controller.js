c2bae3e240e6171d14f588b4b49da5a8a1e57eba
vince <vincedimatatac@gmail.com>
2026-02-25 13:01:17 +0800

    implemented getCart controller function

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,6 +1,27 @@
 import { Cart } from "../models/cart.model.js";
 
-export async function getCart(req, res) {}
+export async function getCart(req, res) {
+  try {
+    const clerkId = req.user.clerkId;
+    let cart = await Cart.findOne({ clerkId: clerkId }).populate(
+      "items.product",
+    );
+
+    // user has no products in cart yet, create a cart
+    if (!cart) {
+      cart = await Cart.create({
+        user: user._id,
+        clerkId: user.clerkId,
+        items: [],
+      });
+    }
+
+    res.status(200).json({ cart });
+  } catch (error) {
+    console.error("Error in getCart", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 export async function addToCart(req, res) {}
 
