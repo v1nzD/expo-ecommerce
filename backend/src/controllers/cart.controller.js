fe4e68eb2cd27a9f2378d2701f3fc3360e8d6a47
vince <vincedimatatac@gmail.com>
2026-02-25 13:14:20 +0800

    implemented addToCart controller function

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 import { Cart } from "../models/cart.model.js";
+import { Product } from "../models/product.model.js";
 
 export async function getCart(req, res) {
   try {
@@ -23,7 +24,58 @@ export async function getCart(req, res) {
   }
 }
 
-export async function addToCart(req, res) {}
+export async function addToCart(req, res) {
+  try {
+    const { productId, quantity = 1 } = req.body;
+
+    // validate product exists and has stock
+    const product = await Product.findById(productId);
+
+    if (!productId) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    if (product.stock < quantity) {
+      return res.status(400).json({ error: "Product has insufficent stock" });
+    }
+
+    // find user's cart
+    const clerkId = req.user.clerkId;
+    let cart = await Cart.findOne({ clerkId: clerkId });
+
+    if (!cart) {
+      cart = await Cart.create({
+        user: user._id,
+        clerkId: user.clerkId,
+        items: [],
+      });
+    }
+
+    // check if item is already in cart => increment item quantity in cart
+    // find the item in cart
+    const existingItem = cart.items.find(
+      (item) => item.product.toString() === productId,
+    );
+    // item already in cart
+    if (existingItem) {
+      // increment quantity by 1
+      const newQuantity = existingItem.quantity + 1;
+      if (product.stock < newQuantity) {
+        return res.status(400).json({ error: "Product has insufficent stock" });
+      }
+    } else {
+      // add new item
+      cart.items.push({ product: productId, quantity });
+    }
+
+    await cart.save();
+
+    res.status(200).json({ message: "Item added to cart", cart });
+  } catch (error) {
+    console.error("Error in addToCart", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 export async function updateCartItem(req, res) {}
 
