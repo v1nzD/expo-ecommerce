df3db8a3c0b8ff259da36e0f29d0e1f7391651f8
vince <vincedimatatac@gmail.com>
2026-02-26 14:26:39 +0800

    imported and installed tailwindcss, react router, tanstack query + setup config for the packages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const App = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="text-red-500 text-3xl">Home Page</h1>
 
       {/* Show the sign-in and sign-up buttons when the user is signed out */}
       <SignedOut>
         <SignInButton />
         <SignUpButton />
       </SignedOut>
-      
+
       {/* Show the user button when the user is signed in */}
       <SignedIn>
         <UserButton />
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -11,10 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
