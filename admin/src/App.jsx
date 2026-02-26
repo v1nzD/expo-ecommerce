a737e6df34173f8a03f87bd075d65d768688f06f
vince <vincedimatatac@gmail.com>
2026-02-26 18:43:38 +0800

    feat: add auth-based routing and dashboard redirect

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,17 @@
 import React from "react";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import { useAuth } from "@clerk/clerk-react";
 
 const App = () => {
+  const { isSignedIn } = useAuth();
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl">Home Page</h1>
-
-      {/* Show the sign-in and sign-up buttons when the user is signed out */}
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-
-      {/* Show the user button when the user is signed in */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <Routes>
+      <Route
+        path="/login"
+        element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />}
+      />
+    </Routes>
   );
 };
 
