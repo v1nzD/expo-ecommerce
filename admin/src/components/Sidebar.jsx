facf42b99a3f3094d800ed74ce2a2cf9808daf49
vince <vincedimatatac@gmail.com>
2026-03-02 17:14:22 +0800

    feat: implemented sidebar component

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import { useUser } from "@clerk/clerk-react";
+import { ShoppingBagIcon } from "lucide-react";
+import { Link, useLocation } from "react-router";
+import { NAVIGATION } from "./Navbar";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const location = useLocation();
+  const { user } = useUser();
+
+  return (
+    <div className="drawer-side is-drawer-close:overflow-visible">
+      <label
+        htmlFor="my-drawer"
+        aria-label="close sidebar"
+        className="drawer-overlay"
+      ></label>
+
+      {/* Parent div made it responsive */}
+      <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-open:w-64 is-drawer-close:w-14">
+        {/* Top */}
+        <div className=" p-4 w-full">
+          <div className="flex items-center gap-3">
+            {/* Shopping Bag Icon */}
+            <div className="size-10 bg-primary rounded-xl flex items-center justify-center shrink-0">
+              <ShoppingBagIcon className="w-6 h-6 text-primary-content" />
+            </div>
+
+            <span className="text-xl font-bold is-drawer-close:hidden">
+              Admin
+            </span>
+          </div>
+        </div>
+
+        {/* Sidebar content */}
+        <ul className="menu w-full grow flex flex-col gap-2">
+          {NAVIGATION.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <li key={item.path}>
+                <Link
+                  to={item.path}
+                  className={`is-drawer-close:tooltip is-drawer-close:tooltip-right ${isActive ? "bg-primary text-primary-content" : ""}`}
+                >
+                  {item.icon}
+                  <span className="is-drawer-close:hidden">{item.name}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* Bottom user info */}
+        <div className="p-4 w-full">
+          <div className="flex items-center gap-3">
+            {/* User's avatar */}
+            <div className="avatar placeholder shrink-0">
+              <img
+                src={user?.imageUrl}
+                alt={user?.name}
+                className="w-10 h-10 rounded-full"
+              />
+            </div>
+
+            {/* User's name */}
+            <div className="flex-1 min-w-0 is-drawer-close:hidden">
+              <p className="text-sm font-semibold truncate">
+                {user?.firstName} {user?.lastName}
+              </p>
+
+              <p className="text-xs opacity-60 truncate">
+                {user?.emailAddresses[0].emailAddress}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
