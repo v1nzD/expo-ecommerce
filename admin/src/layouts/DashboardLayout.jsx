0505321280e77df56d46d96fbe802af2b6cf3017
vince <vincedimatatac@gmail.com>
2026-03-02 11:10:24 +0800

    feat: implemented navbar component

--- admin/src/layouts/DashboardLayout.jsx
+++ admin/src/layouts/DashboardLayout.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
   return (
-    <div>
-      <h1>sidebar</h1>
-      <h1>navbar</h1>
-      <Outlet />
+    <div className="drawer lg:drawer-open">
+      <input
+        type="checkbox"
+        id="my-drawer"
+        className="drawer-toggle"
+        defaultChecked
+      />
+
+      <div className="drawer-content">
+        <Navbar />
+
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+
+      <Sidebar />
     </div>
   );
 };
