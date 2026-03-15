26c993a8eb6504cd8e87d6ed0709e94f5367a93f
vince <vincedimatatac@gmail.com>
2026-03-15 12:53:22 +0800

    feat(products): add useProducts hook with react-query

---
+++ mobile/hooks/useProducts.tsx
@@ -0,0 +1,18 @@
+import { useApi } from "@/lib/api";
+import { Product } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const useProducts = () => {
+  const api = useApi();
+
+  const result = useQuery({
+    queryKey: ["product"],
+    queryFn: async () => {
+      const { data } = await api.get<Product[]>("/products");
+      return data;
+    },
+  });
+  return result;
+};
+
+export default useProducts;
