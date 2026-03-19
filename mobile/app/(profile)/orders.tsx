a82e916dda5a0c61f5a0f0df5703627e8f09b5f9
vince <vincedimatatac@gmail.com>
2026-03-19 16:14:38 +0800

    created (profile)route directory which contains different screens for profile screen

---
+++ mobile/app/(profile)/orders.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OrdersScreen = () => {
+  return (
+    <View>
+      <Text>orders</Text>
+    </View>
+  );
+};
+
+export default OrdersScreen;
