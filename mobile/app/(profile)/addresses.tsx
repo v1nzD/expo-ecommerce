9f973c333023be9f0a6ef416bf5dc5833ac2b6e6
vince <vincedimatatac@gmail.com>
2026-03-25 10:40:48 +0800

    feat(address): add handlers for adding, editing, deleting, and saving addresses with validation and alerts

--- mobile/app/(profile)/addresses.tsx
+++ mobile/app/(profile)/addresses.tsx
@@ -1,10 +1,17 @@
-import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
 import React, { useState } from "react";
 import SafeScreen from "@/components/SafeScreen";
 import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import useAddresses from "@/hooks/useAddresses";
 import AddressesHeader from "@/components/AddressesHeader";
+import { Address } from "@/types";
 
 const AddressesScreen = () => {
   const {
@@ -32,15 +39,100 @@ const AddressesScreen = () => {
     isDefault: false,
   });
 
-  const handleAddAddress = () => {};
+  const handleAddAddress = () => {
+    setShowAddressForm(true);
+    setEditingAddressId(null);
+    setAddressForm({
+      label: "",
+      fullName: "",
+      streetAddress: "",
+      city: "",
+      state: "",
+      zipCode: "",
+      phoneNumber: "",
+      isDefault: false,
+    });
+  };
 
-  const handleEditAddress = () => {};
+  const handleEditAddress = (address: Address) => {
+    setShowAddressForm(true);
+    setEditingAddressId(address._id);
+    setAddressForm({
+      label: address.label,
+      fullName: address.fullName,
+      streetAddress: address.streetAddress,
+      city: address.city,
+      state: address.state,
+      zipCode: address.zipCode,
+      phoneNumber: address.phoneNumber,
+      isDefault: address.isDefault,
+    });
+  };
 
-  const handleDeleteAddress = () => {};
+  const handleDeleteAddress = (addressId: string, label: string) => {
+    Alert.alert("Delete Address", `Are you sure you want to delete ${label}`, [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => deleteAddress(addressId),
+      },
+    ]);
+  };
 
-  const handleSaveAddress = () => {};
+  const handleSaveAddress = () => {
+    if (
+      !addressForm.label ||
+      !addressForm.fullName ||
+      !addressForm.streetAddress ||
+      !addressForm.city ||
+      !addressForm.state ||
+      !addressForm.zipCode ||
+      !addressForm.phoneNumber
+    ) {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
 
-  const handleCloseAddressForm = () => {};
+    if (editingAddressId) {
+      // update an existing address
+      updateAddress(
+        { addressId: editingAddressId, addressData: addressForm },
+        {
+          onSuccess: () => {
+            setShowAddressForm(false);
+            setEditingAddressId(null);
+            Alert.alert("Success", "Address updated successfully");
+          },
+          onError: (error: any) => {
+            Alert.alert(
+              "Error",
+              error?.response?.data?.error || "Failed to update address",
+            );
+          },
+        },
+      );
+    } else {
+      // create new address
+      addAddress(addressForm, {
+        onSuccess: () => {
+          setShowAddressForm(false);
+          Alert.alert("Success", "New address saved successfully");
+        },
+        onError: (error: any) => {
+          Alert.alert(
+            "Error",
+            error?.response?.data?.error || "Failed to save new address",
+          );
+        },
+      });
+    }
+  };
+
+  const handleCloseAddressForm = () => {
+    setShowAddressForm(false);
+    setEditingAddressId(null);
+  };
 
   // todo: create reusable components for loading and error states
   if (isLoading) return <LoadingUI />;
