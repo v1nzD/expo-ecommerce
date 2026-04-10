d64aafdf285a5a3ca7e34b55d743fc32cf261255
Vince <vincedimatatac@gmail.com>
2026-04-10 14:41:55 +0800

    chore: added customerStripeId to user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -55,6 +55,10 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: true,
     },
+    stripeCustomerId: {
+      type: String,
+      default: "",
+    },
     addresses: [addressSchema],
     wishlist: [
       {
