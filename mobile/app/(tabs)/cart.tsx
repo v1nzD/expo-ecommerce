3324c41048b5161c653cd041c9bb87bbe7e5fb2f
Vince <vincedimatatac@gmail.com>
2026-04-10 14:42:47 +0800

    feat: added cart query and mutations

--- mobile/app/(tabs)/cart.tsx
+++ mobile/app/(tabs)/cart.tsx
@@ -1,7 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import useCart from "@/hooks/useCart";
+import { useApi } from "@/lib/api";
+import useAddresses from "@/hooks/useAddresses";
 
 const CartScreen = () => {
+  const api = useApi();
+  const { addToCart, isAddingToCart } = useCart();
+  const { addresses } = useAddresses();
   return (
     <View>
       <Text>CartScreen</Text>
